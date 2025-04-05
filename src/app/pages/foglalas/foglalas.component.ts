4592a64989c138013059b79db6e2ed689fbf3340
h375569 <h375569@stud.u-szeged.hu>
2025-04-05 13:35:37 +0200

    Alap

---
+++ src/app/pages/foglalas/foglalas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foglalas',
+  imports: [],
+  templateUrl: './foglalas.component.html',
+  styleUrl: './foglalas.component.scss'
+})
+export class FoglalasComponent {
+
+}
---
+++ src/app/pages/fooldal/fooldal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fooldal',
+  imports: [],
+  templateUrl: './fooldal.component.html',
+  styleUrl: './fooldal.component.scss'
+})
+export class FooldalComponent {
+
+}

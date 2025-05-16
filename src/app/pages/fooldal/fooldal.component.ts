5520b7b88b90e4e386b75e9ae7e6032b7c15e4ed
D6NNMX <h375569@stud.u-szeged.hu>
2025-05-16 12:32:12 +0200

    spinner

--- src/app/pages/fooldal/fooldal.component.ts
+++ src/app/pages/fooldal/fooldal.component.ts
@@ -4,18 +4,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { Terem } from '../../shared/model/termek';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TeremService } from '../../shared/services/terem.service';
 
 
 @Component({
   selector: 'app-fooldal',
   standalone: true,
-  imports: [CommonModule, MatGridListModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatGridListModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './fooldal.component.html',
   styleUrl: './fooldal.component.scss'
 })
 export class FooldalComponent implements OnInit {
   termek: Terem[] = [];
+  loading = true;
 
   constructor(private teremService: TeremService) {}
 
@@ -23,6 +25,7 @@ export class FooldalComponent implements OnInit {
     this.teremService.getAllTermek().subscribe({
       next: (data) => {
         this.termek = data;
+        this.loading = false;
       },
       error: (err) => {
         console.error('Hiba a termek lekérdezésekor:', err);
